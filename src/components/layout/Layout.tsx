6d3558986553ef483c6c00f49dfa19fbb5e2cc06
[dyad] <vitor.danilo.marques@gmail.com>
2025-08-05 20:18:15 -0300

    [dyad] Corrigindo o layout do menu lateral - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,12 +3,18 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
 import * as React from "react";
+import { cn } from "@/lib/utils";
 
 export function Layout() {
   const [isCollapsed, setIsCollapsed] = React.useState(false);
 
   return (
-    <div className={`grid min-h-screen w-full md:grid-cols-[${isCollapsed ? "68px" : "240px"}_1fr] transition-[grid-template-columns] duration-300 ease-in-out`}>
+    <div 
+      className={cn(
+        "grid min-h-screen w-full transition-[grid-template-columns] duration-300 ease-in-out",
+        isCollapsed ? "md:grid-cols-[68px_1fr]" : "md:grid-cols-[240px_1fr]"
+      )}
+    >
       <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
       <main className="flex flex-1 flex-col gap-4 bg-background p-4 sm:p-6 md:gap-8 md:p-8 overflow-auto">
         <Outlet />
